f0714ed1b2f2765da9d3d56de437d173ca0bd313
PAGUR0 <PAGUR0@PAGUR0>
2024-05-13 14:33:46 +0300

    Добавьте файлы проекта.

---
+++ PP_Lab11_C#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    delegate string GetNextDayDelegate();
+
+    static void Main(string[] args)
+    {
+        List<string> daysOfWeek = new List<string>
+        {
+            "Понедельник",
+            "Вторник",
+            "Среда",
+            "Четверг",
+            "Пятница",
+            "Суббота",
+            "Воскресенье"
+        };
+
+        int currentIndex = 0;
+
+        GetNextDayDelegate getNextDay = () =>
+        {
+            string nextDay = daysOfWeek[currentIndex];
+
+            currentIndex = (currentIndex + 1) % daysOfWeek.Count;
+
+            return nextDay;
+        };
+
+        for (int i = 0; i < 37; i++)
+        {
+            string nextDay = getNextDay();
+            Console.WriteLine($"Следующий день недели: {nextDay}");
+        }
+    }
+}
